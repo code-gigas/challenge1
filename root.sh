862b147e3a28df18c3848bac7a1845995b904858
Kerala-Hackers-Community <87799500+Kerala-Hackers-Community@users.noreply.github.com>
2021-07-22 14:12:23 +0530

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "KHC CTF V1.1"
+sleep 1
+echo "Key Formate(KHC{***_***_***})"
+
+read -p "Enter Your Key: " key
+
+h1="KHC{CTF_V1.1_002}"
+if [ $key == $h1 ]; then
+echo "S0hDe1kwVV9HMFRfMTd9="
+else
+echo "Try Again"
+exit
+fi
+	
